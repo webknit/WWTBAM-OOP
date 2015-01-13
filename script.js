d1714b5764f0e65854e2ac1293ae2f3f9caa0b1b
Shane Prendergast <contact@webknit.co.uk>
2015-01-13 00:31:53 +0000

    fixed the hoisting issue

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var WWTBAM = new Object();
 
 WWTBAM.correctAnswer;
+WWTBAM.Question;
+
 WWTBAM.questionBox = document.getElementById('question');
 WWTBAM.questionNumber = document.getElementById('question-number');
 WWTBAM.answers = document.getElementById('answers');
@@ -12,31 +14,30 @@ WWTBAM.lifeLine = document.getElementById('lifeline');
 
 WWTBAM.Qnum = -1;
 
-WWTBAM.questions = [
-     new Question('Complete this phrase. As sick as a...',['Penguin','Parrot','Puffin','Partridge'] ,'Parrot', 0),
-     new Question("Which legal document states a person's wishes regarding the disposal of their property after death?",['Will','Shall','Would','Should'] ,'Will', 100),
-     new Question('Complete the title of the James Bond film The Man With The Golden...',['Gun','Tooth','Delicious','Eagle'] ,'Gun', 200),
-     new Question('Which of these fruits shares its name with something superior or desirable?',['Apricot','Mango','Grapefruit','Plum'] ,'Plum', 300),
-     new Question('In which sport do two teams pull at the opposite ends of a rope?',['Ice hockey','Basketball','Tug of war','Polo'] ,'Tug of war', 500),
-     new Question('Where would a cowboy wear his chaps?',['On his hands','On his arms','On his legs','On his head'] ,'On his legs', 1000),
-     new Question('Which of these zodiac signs is not represented by an animal that grows horns?',['Taurus','Capricorn','Aquarius','Aries'] ,'Aquarius', 2000),
-     new Question('Sherpas and Gurkhas are native to which country?',['Russia','Ecuador','Nepal','Morocco'] ,'Nepal', 4000),
-     new Question('Prime Minister Tony Blair was born in which country?',['England','Northern Ireland','Scotland','Wales'] ,'Scotland', 8000),
-     new Question('Whose autobiography has the title, "A Long Walk to Freedom"?',['Ranulph Fiennes','Mother Teresa','Nelson Mandela','Mikhail Gorbachev'] ,'Nelson Mandela', 16000),
-     new Question('Duffel coats are named after a town in which country?',['Belgium','Holland','Germany','Austria'] ,'Belgium', 32000),
-     new Question("Complete this stage instruction in Shakespeare's 'The Winter's Tale', 'Exit, pursued by a...'?",['Tiger','Clown','Bear','Dog'] ,'Bear', 64000),
-     new Question("The young of which creature is known as a 'squab'?",['Salmon','Horse','Pigeon','Octopus'] ,'Pigeon', 125000),
-     new Question('Who is the patron saint of Spain?',['Saint James','Saint John','Saint Benedict','Saint Peter'] ,'Saint John', 150000),
-     new Question('Which king was married to Eleanor of Aquitaine?',['Henry I','Henry II','Richard I','Henry V'] ,'Henry II', 1000000)
-];
-
-
-function Question(questionText, answers, answer, score) {
+WWTBAM.Question = function(questionText, answers, answer, score) {
 	this.questionText = questionText;
     this.answers = answers;
     this.answer = answer;
     this.score = score;
 }
+
+WWTBAM.questions = [
+     new WWTBAM.Question('Complete this phrase. As sick as a...',['Penguin','Parrot','Puffin','Partridge'] ,'Parrot', 0),
+     new WWTBAM.Question("Which legal document states a person's wishes regarding the disposal of their property after death?",['Will','Shall','Would','Should'] ,'Will', 100),
+     new WWTBAM.Question('Complete the title of the James Bond film The Man With The Golden...',['Gun','Tooth','Delicious','Eagle'] ,'Gun', 200),
+     new WWTBAM.Question('Which of these fruits shares its name with something superior or desirable?',['Apricot','Mango','Grapefruit','Plum'] ,'Plum', 300),
+     new WWTBAM.Question('In which sport do two teams pull at the opposite ends of a rope?',['Ice hockey','Basketball','Tug of war','Polo'] ,'Tug of war', 500),
+     new WWTBAM.Question('Where would a cowboy wear his chaps?',['On his hands','On his arms','On his legs','On his head'] ,'On his legs', 1000),
+     new WWTBAM.Question('Which of these zodiac signs is not represented by an animal that grows horns?',['Taurus','Capricorn','Aquarius','Aries'] ,'Aquarius', 2000),
+     new WWTBAM.Question('Sherpas and Gurkhas are native to which country?',['Russia','Ecuador','Nepal','Morocco'] ,'Nepal', 4000),
+     new WWTBAM.Question('Prime Minister Tony Blair was born in which country?',['England','Northern Ireland','Scotland','Wales'] ,'Scotland', 8000),
+     new WWTBAM.Question('Whose autobiography has the title, "A Long Walk to Freedom"?',['Ranulph Fiennes','Mother Teresa','Nelson Mandela','Mikhail Gorbachev'] ,'Nelson Mandela', 16000),
+     new WWTBAM.Question('Duffel coats are named after a town in which country?',['Belgium','Holland','Germany','Austria'] ,'Belgium', 32000),
+     new WWTBAM.Question("Complete this stage instruction in Shakespeare's 'The Winter's Tale', 'Exit, pursued by a...'?",['Tiger','Clown','Bear','Dog'] ,'Bear', 64000),
+     new WWTBAM.Question("The young of which creature is known as a 'squab'?",['Salmon','Horse','Pigeon','Octopus'] ,'Pigeon', 125000),
+     new WWTBAM.Question('Who is the patron saint of Spain?',['Saint James','Saint John','Saint Benedict','Saint Peter'] ,'Saint John', 150000),
+     new WWTBAM.Question('Which king was married to Eleanor of Aquitaine?',['Henry I','Henry II','Richard I','Henry V'] ,'Henry II', 1000000)
+];
 
 WWTBAM.nextQuestion = function() {
 
